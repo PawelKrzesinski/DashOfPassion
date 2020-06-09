9c10ab685688ebf474257d2e8d0bd650b039aa0b
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-06-09 20:23:45 +0100

    commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+// async function getCaloriesValue() {
+// 	try {
+// 		let result = await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/9263/information?amount=100&unit=gram", {
+// 			"method": "GET",
+// 			"headers": {
+// 				"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+// 				"x-rapidapi-key": "742c3e4d47msh13052978aa4cb25p129a2fjsn1c21c1c5ad9c"
+// 			}
+// 		})
+// 		let data = await result.json();
+
+// 		return console.log(data);
+	
+	
+// 	} catch(err) {
+// 		console.log(err);
+// 	};
+// }
+// getCaloriesValue();
+
