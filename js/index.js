021937e7312261b8912a16831a1c653abc8044ca
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-06-15 11:44:44 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,119 @@
+const desc = document.querySelectorAll('.recipe__description')
+const descArray = Array.from(desc);
+const copyrights = document.getElementById('copyrights')
+const randomGallery = document.querySelectorAll('.recipe__image')
+
+
+
+class Gallery {
+	async getImages(){
+		try{
+			let result = await fetch("js/recipes.json")
+			let data = await result.json();
+			let recipes = data.items;
+			recipes = recipes.map(item => {
+				const {img} = item;
+				const {title} = item;
+				const {description} = item;
+				return{img, title, description};
+			}) 		
+			return recipes;
+		} catch (error) {
+			console.log(error);
+		} 
+	}
+} 
+
+class GalleryShow {
+	galleryDisplay(recipes){
+		let usedRecipes = [];	
+		randomGallery.forEach(recipe => {
+			let randomIndex = Math.floor(Math.random() * recipes.length);
+			let randomRecipe = recipes[randomIndex];
+			if(recipes.length === 0){
+				recipes = [...usedRecipes];
+				usedRecipes = [];
+			} else {
+				let result = `<img class='random__recipes' src=${randomRecipe.img} />`;
+				recipe.innerHTML = result;
+				let usedRecipe = recipes.splice(randomIndex, 1);
+				usedRecipes.push(usedRecipe);
+			}
+		})
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const galleryImages = new Gallery();
+	const galleryShow = new GalleryShow();
+	
+	galleryImages.getImages().then(recipes => {
+		galleryShow.galleryDisplay(recipes);
+	}) 
+})
+
+let date = new Date;
+let year = date.getFullYear();
+copyrights.innerHTML = `&copy; Copyright ${year}, Dash of Passion!`
+
+
+const backgrounds = [
+	['rgb(198, 198, 198)'],
+	['rgb(216, 216, 216)'],
+	['rgb(171, 171, 171)'],
+	['rgb(232, 232, 232)'],
+	['rgb(185, 185, 185)'],
+	['rgb(203, 203, 203)']
+]
+
+function randomBackground(){
+	descArray.forEach(desc => {
+		desc.style.background = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+	});
+}
+randomBackground();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // async function getCaloriesValue() {
 // 	try {
@@ -18,4 +134,7 @@
 // 	};
 // }
 // getCaloriesValue();
+
+
+
 
